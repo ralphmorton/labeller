9c22b697e92f6955a4f8c2b90be908d1117fbea9
Ralph Morton <ralphmorton@gmail.com>
2024-11-26 09:15:22 +0200

    Support metadata

--- src/example.rs
+++ src/example.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Example {
+    pub metadata: Option<serde_json::Value>,
     pub ground_truth: Option<String>,
     pub text: String
 }
